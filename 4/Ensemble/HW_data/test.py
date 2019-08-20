6ff5503bf8bde5df4369f1f0329b6767c0176767
dizwe <dizwe2716@gmail.com>
2019-08-20 15:09:40 +0900

    4주차

---
+++ 4/Ensemble/HW_data/test.py
@@ -0,0 +1,16 @@
+#%%
+import pickle
+
+with open('./4/Ensemble/HW_data/train_data.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+with open('./4/Ensemble/HW_data/test_data.pkl', 'rb') as f:
+    test = pickle.load(f)
+    
+#%%
+dat
+
+#%%
+data
+
+#%%
